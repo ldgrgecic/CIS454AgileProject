d058e9a56e2ae640f6db7bc16ea817489f6c9347
Brian Welsh <bwelsh@syr.edu>
2019-04-01 17:36:33 -0400

    Added Navigation Menu to MainActivity

--- app/src/main/java/com/example/cis454agileproject/MainActivity.java
+++ app/src/main/java/com/example/cis454agileproject/MainActivity.java
@@ -1,36 +1,91 @@
 package com.example.cis454agileproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class MainActivity extends AppCompatActivity {
+/*
+    *
+    * Home page displayed after user logs in
+    * Displays all nearby requests the user can complete
+    * User's can create new requests through the FAB
+    * Nav Menu links to most app features
+    *
+ */
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get Toolbar, FAB, Drawer, and Nav View
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Create new Post Function will go here", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);                          // set nav drawer
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView nav = findViewById(R.id.nav_view);
+        nav.setNavigationItemSelectedListener(this);
+
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
 
         myRef.setValue("Hello, World!");
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        Intent intent;
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+
+        // TODO: add functionality to nav item buttons
+        switch (id) {
+            case R.id.nav_profile:
+
+                return true;
+            case R.id.nav_create:
+
+                return true;
+
+            case R.id.nav_logout:
+
+                return true;
+        }
+        return true;
     }
 
 }
