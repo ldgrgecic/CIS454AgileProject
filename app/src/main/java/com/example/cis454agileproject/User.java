41ccf126a74c50b9cf46d0a6d946063b15180fd8
theGoliath <lspoor@syr.edu>
2019-04-05 16:36:49 -0400

    Inclussion of Basic Request Class

--- app/src/main/java/com/example/cis454agileproject/User.java
+++ app/src/main/java/com/example/cis454agileproject/User.java
@@ -2,32 +2,31 @@ package com.example.cis454agileproject;
 
 // Creation of all users
 public class User {
-    int userID;
-    int location; // should be a zipcode
-    float timeBank; // all users start at 0
-    String email;
-    String password;
-    String description;
-    String type; // "Provider" vs "Requester" vs "Both", here for now if needed
+    private int userID;
+    private int location; // should be a zipcode
+    private float timeBank; // all users start at 0
+    private String email;
+    private String password;
+    private String description;
+    private String type; // "Provider" vs "Requester" vs "Both", here for now if needed
 
 
     // Create user with all fields
     public User (int userID, String email, String password, String description,
                  int location, float timeBank, String type){
-        this.userID = userID;
-        this.location = location;
-        this.timeBank = timeBank;
-        this.email = email;
-        this.password = password;
-        this.description = description;
-        this.type = type;
+        setUserID(userID);
+        setLocation(location);
+        setTimeBank(timeBank);
+        setEmail(email);
+        setPassword(password);
+        setDescription(description);
+        setType(type);
     }
 
     // Setters and getters for most fields
     public int getUserID(){
         return userID;
     }
-
     public void setUserID(int userID) {
         this.userID = userID;
     }
@@ -35,7 +34,6 @@ public class User {
     public int getLocation(){
         return location;
     }
-
     public void setLocation(int location) {
         this.location = location;
     }
@@ -43,7 +41,6 @@ public class User {
     public float getTimeBank(){
         return timeBank;
     }
-
     public void setTimeBank(float timeBank) {
         this.timeBank = timeBank;
     }
@@ -51,15 +48,15 @@ public class User {
     public String getEmail(){
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
 
-    public String getDescription(){
-        return email;
-    }
+    public void setPassword(String password) { this.password = password; }
 
+    public String getDescription(){
+        return description;
+    }
     public void setDescription(String description) {
         this.description = description;
     }
@@ -67,7 +64,6 @@ public class User {
     public String getType(){
         return type;
     }
-
     public void setType(String type) {
         this.type = type;
     }
