d2654bb6de048287e1a19a7aae57ec3d9dd35631
LukeGrgecic <ldgrgecic@gmail.com>
2019-04-10 14:59:38 -0400

    Updated some aspects of UI I worked on

--- app/src/main/java/com/example/cis454agileproject/ProfileView.java
+++ app/src/main/java/com/example/cis454agileproject/ProfileView.java
@@ -1,7 +1,9 @@
 package com.example.cis454agileproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ProfileView extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class ProfileView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_view);
+    }
+
+
+    public void returnhome(View v) {
+        Intent register = new Intent(this, MainActivity.class);
+        startActivity(register);
     }
 }
