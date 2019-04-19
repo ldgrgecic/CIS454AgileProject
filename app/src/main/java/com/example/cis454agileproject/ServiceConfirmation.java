4d923259832aa64fda4f231a14f92de796aaa13e
LukeGrgecic <ldgrgecic@gmail.com>
2019-04-19 15:41:04 -0400

    I put leave a rating with service confirmation for the time being

--- app/src/main/java/com/example/cis454agileproject/ServiceConfirmation.java
+++ app/src/main/java/com/example/cis454agileproject/ServiceConfirmation.java
@@ -3,7 +3,7 @@ package com.example.cis454agileproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class serviceconfirmation extends AppCompatActivity {
+public class ServiceConfirmation extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
