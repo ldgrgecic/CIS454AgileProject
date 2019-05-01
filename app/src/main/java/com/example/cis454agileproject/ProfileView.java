f3140f59f39ef0825ab1bf470474c401652f0910
theGoliath <lspoor@syr.edu>
2019-05-01 17:26:48 -0400

    Show Profile View

--- app/src/main/java/com/example/cis454agileproject/ProfileView.java
+++ app/src/main/java/com/example/cis454agileproject/ProfileView.java
@@ -1,6 +1,7 @@
 package com.example.cis454agileproject;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -25,7 +26,8 @@ public class ProfileView extends AppCompatActivity {
     private TextView mTimebank;
 
     // Reference to db
-    private DatabaseReference db = FirebaseDatabase.getInstance().getReference("users");
+    private DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+    private DatabaseReference uidRef = rootRef.child("users").child(user.getUid());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,23 +40,40 @@ public class ProfileView extends AppCompatActivity {
         mDescription = findViewById(R.id.cs_descriptionprof_content);
         mTimebank = findViewById(R.id.cs_balanceprof_content);
 
-        db.addValueEventListener(new ValueEventListener() {
+        ValueEventListener valueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                mName.setText(dataSnapshot.child("name").getValue(String.class));
+                mEmail.setText(dataSnapshot.child("email").getValue(String.class));
+                mAddress.setText(dataSnapshot.child("location").getValue(String.class));
+                mDescription.setText(dataSnapshot.child("description").getValue(String.class));
+                mTimebank.setText(Float.toString(dataSnapshot.child("timeBank").getValue(float.class)));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) { }
+        };
+
+        uidRef.addListenerForSingleValueEvent(valueEventListener);
+
+        /*db.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange (DataSnapshot dataSnapshot){
-                User user = dataSnapshot.getValue(User.class);
-                mName.setText(user.getName());
-                mEmail.setText(user.getEmail());
-                mAddress.setText(user.getLocation());
-                mDescription.setText(user.getDescription());
-                mTimebank.setText(String.valueOf(user.getTimeBank()));
+                DataSnapshot userData = dataSnapshot.child("users").child(user.getUid());
+                mName.setText(userData.child("name").getValue(String.class));
+                mEmail.setText(userData.child("email").getValue(String.class));
+                mAddress.setText(userData.child("location").getValue(String.class));
+                mDescription.setText(userData.child("description").getValue(String.class));
+                mTimebank.setText(String.valueOf(userData.child("timebank").getValue(float.class)));
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
                 System.out.println("The read failed: " + databaseError.getCode());
             }
-        });
+        });*/
     }
+
 
 
     public void returnhome(View v) {
