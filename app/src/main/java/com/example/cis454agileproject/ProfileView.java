be5dd0e218108aed40f1f16e86cd34e0e46db880
Megi Shehi <megi.shehi2@gmail.com>
2019-05-01 14:36:52 -0400

    updated profile view

--- app/src/main/java/com/example/cis454agileproject/ProfileView.java
+++ app/src/main/java/com/example/cis454agileproject/ProfileView.java
@@ -4,13 +4,56 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DatabaseError;
 
 public class ProfileView extends AppCompatActivity {
+    // Firebase auth get current user
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+    private TextView mName;
+    private TextView mEmail;
+    private TextView mAddress;
+    private TextView mDescription;
+    private TextView mTimebank;
+
+    // Reference to db
+    private DatabaseReference db = FirebaseDatabase.getInstance().getReference("users");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_view);
+
+        mName = findViewById(R.id.cs_nameprof_content);
+        mEmail = findViewById(R.id.cs_emailprof_content);
+        mAddress = findViewById(R.id.cs_addressprof_content);
+        mDescription = findViewById(R.id.cs_descriptionprof_content);
+        mTimebank = findViewById(R.id.cs_balanceprof_content);
+
+        db.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange (DataSnapshot dataSnapshot){
+                User user = dataSnapshot.getValue(User.class);
+                mName.setText(user.getName());
+                mEmail.setText(user.getEmail());
+                mAddress.setText(user.getLocation());
+                mDescription.setText(user.getDescription());
+                mTimebank.setText(String.valueOf(user.getTimeBank()));
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getCode());
+            }
+        });
     }
 
 
