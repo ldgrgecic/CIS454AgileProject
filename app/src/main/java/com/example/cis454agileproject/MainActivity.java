f65f64a0af4e7c414df54e2b81f98df9e3f6cdcf
Megi Shehi <megi.shehi2@gmail.com>
2019-05-02 17:01:10 -0400

    recycler view variables

--- app/src/main/java/com/example/cis454agileproject/MainActivity.java
+++ app/src/main/java/com/example/cis454agileproject/MainActivity.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
+import android.support.v7.widget.RecyclerView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -27,10 +27,16 @@ import com.google.firebase.database.FirebaseDatabase;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    // Elements for recycler view displaying list of services
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
 
         // Get Toolbar, FAB, Drawer, and Nav View
         Toolbar toolbar = findViewById(R.id.toolbar);
